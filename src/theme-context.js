74b1509d180ba1f00f7a0f7f1704d9012d9f607f
inchara <inchara1092000@gmail.com>
2024-01-19 12:44:35 +0530

    context setup

--- src/theme-context.js
+++ src/theme-context.js
@@ -1,3 +1,20 @@
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 export const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const Context = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode((prevState) => !prevState);
+  };
+  const theme = isDarkMode ? 'dark' : 'light';
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
